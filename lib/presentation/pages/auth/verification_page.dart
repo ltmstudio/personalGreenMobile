f36d1d731c16b248e5e13a491cd8a58deec19a7c
serdar <ibragimowserdar22@gmail.com>
2026-02-03 12:46:55 +0500

    otp deleted from ui

--- lib/presentation/pages/auth/verification_page.dart
+++ lib/presentation/pages/auth/verification_page.dart
@@ -125,7 +125,7 @@ class _VerificationPageState extends State<VerificationPage> {
                 alignment: Alignment.centerLeft,
 
                 child: Text(
-                  '${AppStrings.verificationSend} +7${widget.params.phoneNumber} - otp: ${ widget.params.code}',
+                  '${AppStrings.verificationSend} +7${widget.params.phoneNumber}',
 
                   style: Theme.of(context).textTheme.bodyMedium,
                 ),
